440db30f0db89dae3301f613dc7abc5620e05a9e
Robert Harbul <robert.harbul@gmail.com>
2025-03-27 17:26:49 +0100

    revert darwin flake change

--- nix/darwin/flake.nix
+++ nix/darwin/flake.nix
@@ -65,7 +65,7 @@
 
       # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "aarch64-darwin";
-      nixpkgs.config = import ./nixpkgs-config.nix;
+      nixpkgs.config.allowUnfree = true;
 
       nix-homebrew = {
         enable = true;
